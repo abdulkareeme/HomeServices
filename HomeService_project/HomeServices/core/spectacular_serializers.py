9774f2559d00091e2c8c8f6af9776f09a3bc7ae7
AbdulkareemEd1 <and123456789560@gmail.com>
2023-05-21 14:32:25 +0300

    edit spectacular

--- HomeService_project/HomeServices/core/spectacular_serializers.py
+++ HomeService_project/HomeServices/core/spectacular_serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import User
 class  LoginSpectacular(serializers.ModelSerializer):
     bio = serializers.CharField(max_length = 1000)
+    average_fast_answer = serializers.DurationField()
     class Meta :
         model = User
         fields = ['id','username','email','first_name','last_name','mode','photo','birth_date','date_joined','gender','bio']
